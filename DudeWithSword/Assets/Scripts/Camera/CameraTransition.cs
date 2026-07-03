5cd160fad9b3a7feaa14c87c79f9a2dee6c04f74
dr4k0b <120574803+dr4k0b@users.noreply.github.com>
2026-07-03 18:28:03 +0200

    bettercamera

--- DudeWithSword/Assets/Scripts/Camera/CameraTransition.cs
+++ DudeWithSword/Assets/Scripts/Camera/CameraTransition.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 
 public class CameraTransition : MonoBehaviour
 {
-    public Vector3 offset;
+    [Range(0.0f, 1.0f)]
+    public float sideAngle;
+    [Range(-1.0f, 1.0f)]
+    public float topAngle;
+
+    public float distance;
+
     public float transition;
 
 
@@ -19,9 +25,18 @@ public class CameraTransition : MonoBehaviour
     {
         if (priority)
         {
-            ci.camera.transform.localPosition = Vector3.Lerp(ci.camera.transform.localPosition, offset, transition);
+            ci.camera.transform.localPosition = Vector3.Lerp(ci.camera.transform.localPosition, offset(), transition);
             ci.camera.transform.LookAt(ci.followPlayer.transform);
         }
+    }
+
+
+    private Vector3 offset()
+    {
+        Vector3 side = new Vector3(Mathf.Cos(sideAngle * 2 * Mathf.PI), 0, Mathf.Sin(sideAngle * 2 * Mathf.PI));
+        Vector3 top = new Vector3(0, topAngle * 20, 0);
+
+        return (side + top).normalized * distance;
     }
 
     private void OnTriggerEnter(Collider other)
